e8fdd325dd12b6745670c95225405fec33b46fec
Bergis1610 <89493437+Bergis1610@users.noreply.github.com>
2021-11-26 23:10:50 -0500

    Update Utilities.java

--- compiler/Utilities.java
+++ compiler/Utilities.java
@@ -2,6 +2,18 @@ package compiler;
 
 import java.io.*;
 
+/**
+ * Utilities.java
+ * This is the utilities file that contains the write to file method.
+ * @author Emil Bjørlykke Berglund
+ * @author Adam Fischer
+ * @author Denys Ladden
+ * @version 1.0
+ * Programming project 4
+ * CS322 - Compiler Construction
+ * Fall 2021
+ **/
+
 public class Utilities{
 
     public static void writeFile(byte[] bytearray, String fileName){
